aa4dba9cc1c0395ba5f7929908217f341d76906b
Georg Kunz <kwd@gmx.ch>
2019-05-17 16:19:46 +0200

    Change output of web app

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
   http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome Skaffold! Going to KubeCon")
+    fmt.Fprintf(w, "Welcome Skaffold! Welcome Pipelines")
   })
 
   http.ListenAndServe(":8080", nil)
