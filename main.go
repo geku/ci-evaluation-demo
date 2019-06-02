7d464bbfb8b80374b000f5dc596bd6b6438a1986
Georg Kunz <kwd@gmx.ch>
2019-06-02 12:00:44 +0200

    Update demo app to demonstrate ksimple usage

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
   http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome Skaffold! Welcome Pipelines. ricardo")
+    fmt.Fprintf(w, "Welcome Ksimple! Welcome Pipelines.")
   })
 
   http.ListenAndServe(":8080", nil)
