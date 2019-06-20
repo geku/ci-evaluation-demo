924f7159f8b5f63a4cc782075524b6a3f82f3326
Georg Kunz <kwd@gmx.ch>
2019-06-20 14:44:50 +0200

    Add health endpoint and fix port

--- main.go
+++ main.go
@@ -10,5 +10,9 @@ func main() {
     fmt.Fprintf(w, "Welcome Ksimple! Welcome Pipelines. Welcome review app!")
   })
 
+  http.HandleFunc("/healthz", func (w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "ok")
+  })
+
   http.ListenAndServe(":8080", nil)
 }
